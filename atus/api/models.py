37f4604b3e34494e5fb77a48062b030b95b17bd8
PJ Passalacqua <pjpassa@gmail.com>
2015-07-16 18:34:53 -0400

    inital models

--- atus/api/models.py
+++ atus/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from jsonfield import JSONField
 
 # Create your models here.
+class Respondent(models.Model):
+    statistical_weight = models.FloatField()
+    variables = JSONField()
+
+
+class TimeSpent(models.Model):
+    respondent = models.ForeignKey(Respondent)
+    activity = models.CharField(max_length=7)
+    activity_label = models.CharField(max_length=100)
+    time_spent = models.IntegerField()
--- atus/atus/settings.py
+++ atus/atus/settings.py
@@ -36,9 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
-    #'rest_framework',
-    #'api',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -103,7 +103,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()

92b22e140000ba76c0ede44b595c7edf89d65509
pnitto <pnitto18@gmail.com>
2015-07-17 09:55:26 -0400

    timespent urls, timespent views, timespentserializer

--- atus/api/models.py
+++ atus/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from jsonfield import JSONField
+import django_filters
 
 
 
@@ -15,3 +16,14 @@ class TimeSpent(models.Model):
     time_spent = models.IntegerField()
 
 
+
+"""class TimesSpentFilter(django_filters.FilterSet):
+    # You can set up a filter on a model based on the fields.
+
+    class Meta:
+        model = TimeSpent
+        fields = ["respondent", "activity", "activity_label", "time_spent"]
+# can set up a filter by passing a dictionary to the fields attribute.
+        # also have an order_by attribute to control the display
+        fields = {'price': ['lt', 'gt'],
+                  'release_date': ['exact'],"""

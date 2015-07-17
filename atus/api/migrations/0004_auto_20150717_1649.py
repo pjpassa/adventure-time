08943eba091bd1bbf273860b8c911099fb8473a5
PJ Passalacqua <pjpassa@gmail.com>
2015-07-17 15:25:43 -0400

    updated model and created data migrations

--- atus/api/migrations/0004_auto_20150717_1649.py
+++ atus/api/migrations/0004_auto_20150717_1649.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import csv
 
 from django.db import models, migrations
+from api.models import Respondent
 
 
 def load_summary_data(x, y):
-    raise BaseException()
-
+    with open("../data/atussum_2014/atussum_2014.dat") as file:
+        data = csv.DictReader(file)
+        for line in data:
+            resp = Respondent.objects.get_or_create(tu_case_id=line.pop("tucaseid"))[0]
+            resp.activity_time_spent = line
+            resp.save()
 
 
 class Migration(migrations.Migration):
--- atus/api/models.py
+++ atus/api/models.py
@@ -4,9 +4,10 @@ import django_filters
 
 
 class Respondent(models.Model):
+    tu_case_id = models.CharField(max_length=25)
     statistical_weight = models.FloatField()
     variables = JSONField()
-    activity_time_spent = JSONField()
+    activity_time_spent = JSONField(blank=True)
 
 
 class TimeSpent(models.Model):

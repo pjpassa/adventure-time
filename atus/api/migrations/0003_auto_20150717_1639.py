7ba96e2a7b7306d48fd3b81c39db83c720421a11
PJ Passalacqua <pjpassa@gmail.com>
2015-07-17 12:49:30 -0400

    added empty migrations

---
+++ atus/api/migrations/0003_auto_20150717_1639.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0002_respondent_activity_time_spent'),
+    ]
+
+    operations = [
+    ]

653db8f7759e38af15be1453e8bf7ec42b80829b
erikdvoss <erik@Eriks-MacBook-Air.local>
2015-07-18 16:00:53 -0400

    ...

--- atus/api/admin.py
+++ atus/api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from api.models import Respondent, TimeSpent
+from api.models import Respondent
 
 admin.site.register(Respondent)
 admin.site.register(TimeSpent)

9a3777d91e1e6cdfe11e7d41c9baa511ff76169d
erikdvoss <erik@Eriks-MacBook-Air.local>
2015-07-19 18:18:41 -0400

    removed bad url

--- atus/atus/urls.py
+++ atus/atus/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     url(r'^activities/(?P<pk>\d+)/$', views.ActivityView.as_view(), name="activity"),
     url(r'^respondents/(?P<pk>\d+)/', views.RespondentDetailView.as_view(), name="respondent_detail"),
     url(r'^respondents/$', views.RespondentListView.as_view(), name="respondent_list"),
-    url(r'^activity/(?P<pk>t\d{6})/', views.ActivityDetailView.as_view(), name="activity_detail"),
 ]
